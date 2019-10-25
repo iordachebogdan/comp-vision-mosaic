01ee36b312cb9c29d9a40030b76d6c722353cfde
iordachebogdan <iordache_bogdan1998@yahoo.com>
2019-10-25 22:08:57 +0300

    Reformat

--- builder/mosaic_builder.py
+++ builder/mosaic_builder.py
@@ -16,20 +16,26 @@ class MosaicBuilder:
         add_checkpoint = self.parameters.add_checkpoint_perc
         next_checkpoint = self.parameters.add_checkpoint_perc
         result = np.full(self.parameters.image.shape, 255, np.uint8)
-        used_images = np.full((self.parameters.num_pieces_vertical,
-                               self.parameters.num_pieces_horizontal),
-                              -1)
+        used_images = np.full(
+            (
+                self.parameters.num_pieces_vertical,
+                self.parameters.num_pieces_horizontal,
+            ),
+            -1,
+        )
         for i in range(0, self.parameters.num_pieces_vertical):
             for j in range(0, self.parameters.num_pieces_horizontal):
                 mean_fragment = np.mean(
                     self.parameters.image[
-                        i * self.parameters.height_small:
-                            (i+1) * self.parameters.height_small,
-                        j * self.parameters.width_small:
-                            (j+1) * self.parameters.width_small,
-                        ...
+                        i
+                        * self.parameters.height_small : (i + 1)
+                        * self.parameters.height_small,
+                        j
+                        * self.parameters.width_small : (j + 1)
+                        * self.parameters.width_small,
+                        ...,
                     ],
-                    axis=(0, 1)
+                    axis=(0, 1),
                 )
 
                 if self.parameters.criterion == "euclid":
@@ -40,55 +46,57 @@ class MosaicBuilder:
                     selected_image = self.__choose_random()
 
                 result[
-                    i * self.parameters.height_small:
-                        (i+1) * self.parameters.height_small,
-                    j * self.parameters.width_small:
-                        (j+1) * self.parameters.width_small,
-                    ...
+                    i
+                    * self.parameters.height_small : (i + 1)
+                    * self.parameters.height_small,
+                    j
+                    * self.parameters.width_small : (j + 1)
+                    * self.parameters.width_small,
+                    ...,
                 ] = selected_image
 
-                percentage = (i * self.parameters.num_pieces_horizontal + j) /\
-                    (self.parameters.num_pieces_horizontal *
-                     self.parameters.num_pieces_vertical)
+                percentage = (i * self.parameters.num_pieces_horizontal + j) / (
+                    self.parameters.num_pieces_horizontal
+                    * self.parameters.num_pieces_vertical
+                )
                 sys.stdout.write("\r")
                 sys.stdout.write("Progress: %d%%" % (int(percentage * 100)))
                 sys.stdout.flush()
                 if percentage >= next_checkpoint:
-                    next_checkpoint = \
-                        min(next_checkpoint + add_checkpoint, 1)
+                    next_checkpoint = min(next_checkpoint + add_checkpoint, 1)
                     cv.imwrite(
-                        self.parameters.results_dir + str(int(100*percentage))
-                        + "." + self.parameters.small_images_type,
-                        result
+                        self.parameters.results_dir
+                        + str(int(100 * percentage))
+                        + "."
+                        + self.parameters.small_images_type,
+                        result,
                     )
 
         sys.stdout.write("\n")
         print("Writing solution ...")
-        cv.imwrite(self.parameters.results_dir + "result."
-                   + self.parameters.small_images_type, result)
+        cv.imwrite(
+            self.parameters.results_dir + "result." + self.parameters.small_images_type,
+            result,
+        )
         print("Done!")
 
     def __compute_means(self):
-        return [np.mean(image, axis=(0, 1))
-                for image in self.parameters.small_images]
+        return [np.mean(image, axis=(0, 1)) for image in self.parameters.small_images]
 
-    def __choose_euclid(self,
-                        i: int, j: int,
-                        mean_fragment: np.ndarray,
-                        used_images: np.ndarray) -> np.ndarray:
+    def __choose_euclid(
+        self, i: int, j: int, mean_fragment: np.ndarray, used_images: np.ndarray
+    ) -> np.ndarray:
         best = -1
         best_mean = np.zeros(3)
 
         for idx in range(0, len(self.means)):
             curr_mean = self.means[idx]
-            if best == -1 or \
-                    np.linalg.norm(mean_fragment-curr_mean) < \
-                    np.linalg.norm(mean_fragment-best_mean):
-                if self.parameters.different and \
-                        i > 0 and used_images[i-1, j] == idx:
+            if best == -1 or np.linalg.norm(mean_fragment - curr_mean) < np.linalg.norm(
+                mean_fragment - best_mean
+            ):
+                if self.parameters.different and i > 0 and used_images[i - 1, j] == idx:
                     continue
-                if self.parameters.different and \
-                        j > 0 and used_images[i, j-1] == idx:
+                if self.parameters.different and j > 0 and used_images[i, j - 1] == idx:
                     continue
                 best = idx
                 best_mean = curr_mean
--- config/parameters.py
+++ config/parameters.py
@@ -24,27 +24,28 @@ class Parameters:
 
             self.image = cv.imread(
                 self.image_path,
-                cv.IMREAD_GRAYSCALE if self.grayscale else cv.IMREAD_COLOR
+                cv.IMREAD_GRAYSCALE if self.grayscale else cv.IMREAD_COLOR,
             )
             self.small_images = [
-                    cv.imread(
-                        img,
-                        cv.IMREAD_GRAYSCALE
-                        if self.grayscale
-                        else cv.IMREAD_COLOR
-                    )
-                    for img in glob.glob(
-                        self.small_images_dir + "*." + self.small_images_type)
+                cv.imread(
+                    img, cv.IMREAD_GRAYSCALE if self.grayscale else cv.IMREAD_COLOR
+                )
+                for img in glob.glob(
+                    self.small_images_dir + "*." + self.small_images_type
+                )
             ]
             self.__compute_dimensions()
             if self.show_small_images:
                 self.__print_small_images()
 
     def __compute_dimensions(self):
-        self.num_pieces_vertical = int(np.ceil(
-            (self.image.shape[0] / self.image.shape[1]) *
-            (self.small_images[0].shape[1] / self.small_images[0].shape[0]) *
-            self.num_pieces_horizontal))
+        self.num_pieces_vertical = int(
+            np.ceil(
+                (self.image.shape[0] / self.image.shape[1])
+                * (self.small_images[0].shape[1] / self.small_images[0].shape[0])
+                * self.num_pieces_horizontal
+            )
+        )
         self.height_small = self.small_images[0].shape[0]
         self.width_small = self.small_images[0].shape[1]
         self.height = self.num_pieces_vertical * self.height_small
@@ -54,9 +55,9 @@ class Parameters:
     def __print_small_images(self):
         for i in range(0, 10):
             for j in range(0, 10):
-                plt.subplot(10, 10, i*10 + j + 1)
+                plt.subplot(10, 10, i * 10 + j + 1)
                 # swap channels BGR (opencv) to RGB (matplotlib)
-                im = self.small_images[i*10 + j].copy()
+                im = self.small_images[i * 10 + j].copy()
                 im = im[:, :, [2, 1, 0]]
                 plt.imshow(im)
         plt.show()
